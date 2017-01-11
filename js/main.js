8a59db9ef1e693f08273afdedf2b1154ffa3a8cc
Renate Hartog <renate@Renates-MacBook-Pro.local>
2017-01-10 20:04:05 -0800

    moved function

--- js/main.js
+++ js/main.js
@@ -125,3 +125,14 @@ function update() {
 
     }
 }
+function collectStar (player, star) {
+
+    // Removes the star from the screen
+    star.kill();
+
+    //  Add and update the score
+    score += 10;
+    scoreText.text = 'Score: ' + score;
+
+
+}
