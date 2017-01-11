628aeb1f9bd3e00e88da7b471d2aab66c73a900d
jrhartog <jrhartog@gmail.com>
2017-01-10 18:15:50 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('orb', 'assets/orb.png');
+  game.load.image('orb', 'assets/orb.jpg');
 }
 
 function create() {
