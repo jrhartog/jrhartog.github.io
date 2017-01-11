e48de7aefa31b56c6139ca3beb88ffcb0e97db77
Renate Hartog <renate@Renates-MacBook-Pro.local>
2017-01-10 22:12:25 -0800

    jump less high

--- js/main.js
+++ js/main.js
@@ -121,7 +121,7 @@ function update() {
     //  Allow the player to jump if they are touching the ground.
     if (cursors.up.isDown && player.body.touching.down && hitPlatform)
     {
-        player.body.velocity.y = -350;
+        player.body.velocity.y = -150;
     }
 
 }
