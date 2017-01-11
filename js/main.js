e04b47caa5558e7ce35c398611b40f215ad9cb63
Renate Hartog <renate@Renates-MacBook-Pro.local>
2017-01-10 18:14:41 -0800

    additions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('orb', 'assets/orb.png');
 }
 
 function create() {
+  game.add.sprite(0,0,'orb');
 }
 
 function update() {
